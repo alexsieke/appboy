da9114f1a17aa0032d265b502f68f8a90668f742
Alex Sieke <asieke@gmail.com>
2014-07-03 16:18:58 -0700

    Finished Script

--- hotdeals/schedule.php
+++ hotdeals/schedule.php
@@ -1,5 +1,16 @@
 <?php
 
+date_default_timezone_set("GMT");
+
+/********************************
+SET UP CAMPAIGN NAME VARIABLES
+*********************************/
+
+$campaign_name = "hotdeals";
+
+$ts = date("Y-m-d");
+$handle = fopen("$campaign_name"."-".$ts.".txt","a+");
+
 /*Create Deals Array*/
 /*
 $deals[deal_id] = array(6)
@@ -12,23 +23,27 @@ $deals[deal_id] = array(6)
 
 */
 
+/*read the file into $file object and parse the rows into deals array*/
 $file = file("deals.csv");
 foreach($file as $line){
 	$arr = explode("\",\"",substr(trim($line), 1, -1));
-
 	$deals[$arr[0]] = $arr;
 }
 
 /*set global variables*/
 $total_pushes = 0;
+foreach($deals as $deal_id => $deal_arr){
+	$iphone_count[$deal_id] = 0;
+	$android_count[$deal_id] = 0;
+}
 
-$COMPANY_SECRET = "";
 
 /*Read in people / deals
 0 = person_id
 1 = app type
 2 = deal_id
 */
+
 
 ini_set('memory_limit', '1024M');
 $file = file("people.csv");
@@ -76,61 +91,83 @@ foreach($android_people as $deal_id => $people_array){
 function send_iphone($deal_id, $people_array){
 	//sends an iphone push notif for $deal_id to people in $people_array
 	
-	global $total_pushes, $deals;
+	global $total_pushes, $deals, $handle;
+
+	foreach($people_array as $person){
+		fwrite($handle,"$person\t$deal_id\t".date("Y-m-d G:i:s")."\n");	
+	}
 
 	$total_pushes += count($people_array);
 
-	echo "iPhone Push Sent\t$total_pushes\n";
+	$data = array(
+		"company_secret" => "x5hhwgGylFYruy9aR_C-lQ", 
+		"app_group_id" => "eb3c7a73-3628-4807-80bb-7e833dae05da", 
+		"external_user_ids" => $people_array, 
+		"messages" => array(
+			"apple_push" => array(
+				"alert" => $deals[$deal_id][2] /*[2] = merchant name, change to [4] for short_title*/,
+				"extra" => array(
+					"url" => "ls://daily/$deal_id"
+					)
+				)
+			)
+		);
+
+	$data_string = json_encode($data, JSON_UNESCAPED_SLASHES);
+
+	$ch = curl_init('https://api.appboy.com/messages/send');
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+	    'Content-Type: application/json',                                                                                
+	    'Content-Length: ' . strlen($data_string))                                                                       
+	);
+
+	$result = curl_exec($ch);
+
 }
 
 function send_android($deal_id, $people_array){
-	//sends an iphone push notif for $deal_id to people in $people_array
+	//sends an android push notif for $deal_id to people in $people_array
 	
-	global $total_pushes, $deals;
+	global $total_pushes, $deals, $handle;
+
+	foreach($people_array as $person){
+		fwrite($handle,"$person\t$deal_id\t".date("Y-m-d G:i:s")."\n");	
+	}
 
 	$total_pushes += count($people_array);
 
-	echo "Android Push Sent\t$total_pushes\n";
+	$data = array(
+		"company_secret" => "x5hhwgGylFYruy9aR_C-lQ", 
+		"app_group_id" => "eb3c7a73-3628-4807-80bb-7e833dae05da", 
+		"external_user_ids" => $people_array, 
+		"messages" => array(
+			"android_push" => array(
+				"alert" => $deals[$deal_id][2] /*[2] = merchant name, change to [4] for short_title*/,
+				"extra" => array(
+					"url" => "ls://daily/$deal_id"
+					)
+				)
+			)
+		);
+
+	$data_string = json_encode($data, JSON_UNESCAPED_SLASHES);
+
+	$ch = curl_init('https://api.appboy.com/messages/send');
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+	    'Content-Type: application/json',                                                                                
+	    'Content-Length: ' . strlen($data_string))                                                                       
+	);
+
+	$result = curl_exec($ch);
+
 }
 
 
-/*Android Push Notifs*/
-/*curl -X POST -H "Content-Type: application/json" -d "{\"company_secret\":\"x5hhwgGylFYruy9aR_C-lQ\",\"app_group_id\":\"eb3c7a73-3628-4807-80bb-7e833dae05da\",\"external_user_ids\":[\"8151533\"],\"messages\":{\"android_push\":{\"alert\":\"50% of 18 holes of golf\",\"extra\":{\"deal_id\":\"1098135\",\"title\":\"Raspberry Falls\"}}}}" https://api.appboy.com/messages/send*/
-
-/*curl -X POST -H "Content-Type: application/json" -d "{\"company_secret\":\"x5hhwgGylFYruy9aR_C-lQ\",\"app_group_id\":\"eb3c7a73-3628-4807-80bb-7e833dae05da\",\"external_user_ids\":[\"8151533\",\"174534995\"],\"messages\":{\"android_push\":{\"alert\":\"Vapiano\",\"extra\":{\"url\":\"ls://daily/1145213\"}}}}" https://api.appboy.com/messages/send*/
-
-// $data = array(
-// 	"company_secret" => "x5hhwgGylFYruy9aR_C-lQ", 
-// 	"app_group_id" => "eb3c7a73-3628-4807-80bb-7e833dae05da", 
-// 	"external_user_ids" => array("8151533"), 
-// 	"messages" => array(
-// 		"android_push" => array(
-// 			"alert" => "50% of Hot Brunch",
-// 			"TITLE_KEY" => "How you like my main",
-// 			"extra" => array(
-// 				"url" => "ls://daily/1186902",
-// 				"TITLE_KEY" => "How you like me now"
-// 				)
-// 			)
-// 		)
-// 	);
-
-// $data_string_old = "{\"company_secret\":\"x5hhwgGylFYruy9aR_C-lQ\",\"app_group_id\":\"eb3c7a73-3628-4807-80bb-7e833dae05da\",\"external_user_ids\":[\"8151533\"],\"messages\":{\"android_push\":{\"alert\":\"50% of Hot Brunch\",\"extra\":{\"url\":\"ls://daily/1186902\"}}}}";
-// $data_string = json_encode($data, JSON_UNESCAPED_SLASHES);
-
-// echo "$data_string_old\n\n$data_string\n\n";
-
-// $ch = curl_init('https://api.appboy.com/messages/send');
-// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-// curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-//     'Content-Type: application/json',                                                                                
-//     'Content-Length: ' . strlen($data_string))                                                                       
-// );
-
-// $result = curl_exec($ch);
-
-// echo "$result\n";
 
 ?>
